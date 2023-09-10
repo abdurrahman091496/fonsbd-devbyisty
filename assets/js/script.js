34032f74deaa2a8fe787391bbb4b53bbb4cfe224
Abdur Rahman Isty <132696955+abdurrahman091496@users.noreply.github.com>
2023-09-10 17:29:28 +0600

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,25 @@
 let closeicon = function(icon) {
     icon.classList.toggle('fa-times')
 }
+
+
+// scroll top 
+const scrollBtn = document.querySelector(".scroll-top");
+
+refreshButtonVisibility = () => {
+    if (document.documentElement.scrollTop <= 280){
+        scrollBtn.style.display="none";
+    } else {
+        scrollBtn.style.display="block"; 
+    }
+};
+
+refreshButtonVisibility();
+
+scrollBtn.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+})
+
+document.addEventListener("scroll", (e) => {
+    refreshButtonVisibility();
+});
